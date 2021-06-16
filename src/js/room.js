e659fc780b7c607999f772dfa1331f5c3e35c73c
Andrea <dredre1997@gmail.com>
2021-06-16 12:23:12 +0300

    Updated Chat responsivness

--- src/js/room.js
+++ src/js/room.js
@@ -3,5 +3,8 @@ $(document).on("keydown", (e) => {
       e.preventDefault();
       $("#form-chatting").click();
     } else if (e.keyCode === 13) e.preventDefault();
+
+    $("#form-chatting-media").text(language.send);
+    $("#message-input-media").attr("placeholder", language.write);
   });
   
